4fbdb77ff476b80af9708bd5c8d91164da62c31f
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-02-05 12:20:08 +0100

    sorting options added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    Time: {{time}}
+    <div class="form">
+      <select v-model="mode">
+        <option :value="'random'">Random</option>
+        <option :value="'groups'">Groups</option>
+        <option :value="'wmi'">Window, Middle, Ile</option>
+        <option :value="'inorder'">Front to Back</option>
+        <option :value="'perfect'">Perfect</option>
+        <option :value="'custom'">Custom (open Dev Console)</option>
+      </select>
+      <button @click="restartGame">Run</button>
+    </div>
     <div class="container">
       <img src="./assets/plane.png">
       <div 
@@ -10,12 +20,14 @@
         v-for="(person, i) in persons"
         :key="`sit${person.id}`">
       </div>
+      <div class="time">{{time}}</div>
+
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 import { sortBy, reverse } from 'lodash';
 const ileY = 417;
 
@@ -42,66 +54,18 @@ function shuffle(array) {
 export default {
   name: 'app',
   data() {
-    const persons = [];
-    let id = 0;
-    for (let x = 0; x < 16; x++) {
-      for (let y = 0; y < 6; y++) {
-        persons.push({ id:++id, row:x, column:y, x:-id, y:null, sitting: false, unloading: false,
-          trySit() {
-            if (this.x == this.row) {
-              if (!this.unloading) {
-
-                this.unloading = true;
-                return;
-              }
-              this.sitting = true;
-              this.y = this.column;
-            }
-          },
-          tryMove(persons) {
-            if (this.sitting) return;
-            if (this.unloading) return;
-            const nextPerson = persons.filter(it => !it.sitting).find(it => it.x == this.x+1)
-            if (!nextPerson) {
-              this.x++;
-              return;
-            }
-          } 
-        })
-      }
-    }
-    shuffle(persons)
-    persons.forEach((it, i) => it.x=-i);
     return {
       time: 0,
-      persons: shuffle(persons),
+      persons: [],
+      sortFunction: () => 0,
+      mode: "Random",
     }
   },
   created() {
-    const that = this;
-    function findPersonInfront(person, persons) {
-      const ret =  that.persons
-        .filter(it => !it.sitting)
-        .find(it => {
-          return person.x - it.x < 2
-          })
-      return ret;
-    }
-    const gameLoop = () => {
-      const sorted = reverse(sortBy(that.persons.filter(it => !it.sitting), 'x'))
-      sorted.forEach((person, i) => {
-        person.trySit()
-        person.tryMove(that.persons)
-      })
-      that.time++
-    }
-    function loop() {
-      if (that.walkingPersons.length != 0) {
-        gameLoop()
-        setTimeout(loop, 1000)
-      }
-    };
-    loop();
+    this.createPersons()
+      this.startGameLoop();
+    console.log("set window.sortFunction = function(person) { return person.row } ")
+    console.log("person hat Properties .row, .column. Your Function should return a Number. Persons with smaller Numbers come first")
   },
   computed: {
     walkingPersons() {
@@ -120,16 +84,113 @@ export default {
     },
     topDistForColumn(column) {
       if (column === null) return ileY;
-      if (column >= 3) return ileY+(column-2)*70;
-      return ileY-(column+1)*70
+      //if (column <= 3) return ileY-(column+1)*70;
+      return ileY+3*70-(column + (column >= 3 ? 1 : 0))*70
+    },
+    createPersons() {
+      let persons = [];
+      let id = 0;
+      for (let x = 0; x < 16; x++) {
+        for (let y = 0; y < 6; y++) {
+          persons.push({ id:++id, row:x, column:y, x:-id, y:null, sitting: false, unloading: false,
+            trySit() {
+              if (this.x == this.row) {
+                if (!this.unloading) {
+
+                  this.unloading = true;
+                  return;
+                }
+                this.sitting = true;
+                this.y = this.column;
+              }
+            },
+            tryMove(persons) {
+              if (this.sitting) return;
+              if (this.unloading) return;
+              const nextPerson = persons.filter(it => !it.sitting).find(it => it.x == this.x+1)
+              if (!nextPerson) {
+                this.x++;
+                return;
+              }
+            } 
+          })
+        }
+      }
+      persons = shuffle(persons);
+      persons = sortBy(persons, this.sortFunction)
+      persons.forEach((it, i) => it.x=-i);
+      this.persons = persons
+    },
+    startGameLoop() {
+      const that = this;
+      
+      const gameLoop = () => {
+        const sorted = reverse(sortBy(that.persons.filter(it => !it.sitting), 'x'))
+        sorted.forEach((person, i) => {
+          person.trySit()
+          person.tryMove(that.persons)
+        })
+        that.time++
+      }
+      function loop() {
+        if (that.walkingPersons.length != 0) {
+          gameLoop()
+          setTimeout(loop, 1000)
+        }
+      };
+      loop();
+    },
+    restartGame() {
+      const functions = {
+        "inorder": (it) => it.id,
+        "groups": (it) => {
+          if (it.row > 15) return 0;
+          if (it.row > 10) return 1;
+          return 2;
+        },
+        "random": (it) => 0,
+        "wmi": (it) => {
+          if (it.column == 0 || it.column == 5) return 0;
+          if (it.column == 1 || it.column == 4) return 1;
+          return 2;
+        },
+        "custom": (it) => window.sortFunction(it),
+        "perfect": (it) => {
+          const even = it%2
+          if (even) {
+            return it.row;
+          } else {
+            return it.row*3000;
+          }
+        }
+      }
+      this.sortFunction = functions[this.mode]
+      this.createPersons();
+      console.log("sorted:")
+      console.table(JSON.parse(JSON.stringify(this.persons)))
+    
     }
   }
 }
 </script>
 
 <style>
+#app {
+  display: flex;
+  flex-direction: column;
+}
 .container {
   position: relative;
+  flex: 1;
+  align-self: center;
+  overflow: hidden;
+}
+.time {
+  position: absolute;
+  font-size: 50px;
+  left: 50%;
+  bottom: 30px;
+  margin-left: -60px;
 }
 .person {
   transition: all 1s;
@@ -143,5 +204,9 @@ export default {
   background-image: url('./assets/person-sitting.png');
   background-position-x: 35px;
   
+}
+html, body {
+  margin: 0;
+  padding: 0;
 }
 </style>
